d059ceb80b9f9d22ff6d38f94e7209395caaffb4
Emilia Mäkelä <bgr887@myy.haaga-helia.fi>
2022-12-07 20:35:03 +0200

    First and last commit

---
+++ website/models.py
@@ -0,0 +1,34 @@
+from . import db
+from flask_login import UserMixin
+from sqlalchemy.sql import func
+from sqlalchemy.orm import relationship, backref
+
+class Recipe(db.Model, ):
+    
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    title = db.Column(db.String(100))
+    instructions = db.Column(db.Text)
+    price = db.Column(db.Float)
+    people = db.Column(db.Integer)
+    time = db.Column(db.Integer)
+    ingredients = db.relationship("Ingredient", backref="recipe")
+
+    def __repr__(self):
+        return f'<Recipe "{self.title}">'
+
+class Ingredient(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.Text)
+    recipe_id = db.Column(db.Integer, db.ForeignKey("recipe.id"))
+
+    def __repr__(self):
+        return f'<Comment "{self.content[:20]}...">'
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(150), unique=True)
+    password = db.Column(db.String(150))
+    first_name = db.Column(db.String(150))
+    recipes = db.relationship('Recipe')
+
---
+++ website/views.py
@@ -0,0 +1,76 @@
+from flask import Blueprint, render_template, redirect,request, flash, jsonify, url_for
+from flask_login import login_required, current_user
+from .models import Recipe, Ingredient
+from . import db
+import json
+
+views = Blueprint('view', __name__)
+
+
+@views.route('/')
+@login_required
+def home():
+    # get all the recipes form data
+    recipes = Recipe.query.all()
+    #count the price for one serving
+    for recipe in recipes:
+        recipe.price_per_person = recipe.price / recipe.people
+    return render_template("home.html", user=current_user, recipes=recipes)
+
+
+@views.route("/addrecipe", methods=["GET", "POST"])
+def add_name():
+    if request.method == "POST":
+        # get the form data
+        title = request.form.get("title")
+        instructions = request.form.get("instructions")
+        price = request.form.get("price")
+        people = request.form.get("people")
+        time = request.form.get("time")
+        
+        #check that input data type is valid
+        if not people.isdigit(): 
+            flash("Please use a valid number", category='error')
+        elif "," in price:
+            flash("Please use a dot (.) as the decimal separator for the price", category='error')
+        elif "," in time:
+            flash("Please add time in minutes not in hours", category='error')
+        else:
+
+            # add the recipe to the database
+            recipe = Recipe(title=title, instructions=instructions, price=price,people=people, time=time)
+            db.session.add(recipe)
+            db.session.commit()
+
+            # redirect to the next page in the recipe creation process
+            return redirect(url_for("view.addingredients", recipe_id=recipe.id))
+
+    # render the form page
+    return render_template("addrecipe.html", user=current_user)
+
+@views.route("/addingredients/<int:recipe_id>", methods=["GET", "POST"])
+def addingredients(recipe_id):
+    # get the recipe from the database
+    recipe = Recipe.query.get(recipe_id)
+
+    if request.method == "POST":
+        # get the form data
+        ingredient_name = request.form.get("name")
+        # add the ingredient to the database
+        ingredient = Ingredient(name=ingredient_name, recipe_id=recipe_id)
+        db.session.add(ingredient)
+        db.session.commit()
+        flash("Ingredient saved", category='info')
+    return render_template('addingredients.html', user=current_user,)
+
+@views.route('/delete-recipe')
+def delete_recipe():
+    # get the recipe from the database
+    recipe_id = request.args.get('recipe_id')
+    recipe = Recipe.query.get(recipe_id)
+    # delete recipe
+    db.session.delete(recipe)
+    db.session.commit()
+    return redirect('/')
+
+
